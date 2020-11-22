db899aa65e89ebc3bf301a30759ea71971cedb7f
YoEight <yo.eight@gmail.com>
2020-11-22 23:38:09 +0100

    Implement main loop.

---
+++ src/input.rs
@@ -0,0 +1,138 @@
+use std::io::{self, Stdin, Stdout, Write};
+use termion::cursor::DetectCursorPos;
+use termion::event::Key;
+use termion::input::{Keys, TermRead};
+
+pub enum Input {
+    String(String),
+    Command(String),
+    Exit,
+}
+
+pub struct Inputs {
+    buffer: String,
+    offset: u16,
+    start_pos: u16,
+    keys: Keys<Stdin>,
+}
+
+impl Inputs {
+    pub fn new() -> Self {
+        let keys = io::stdin().keys();
+
+        Inputs {
+            keys,
+            buffer: String::new(),
+            offset: 0,
+            start_pos: 3,
+        }
+    }
+
+    pub fn await_input(&mut self, stdout: &mut Stdout) -> io::Result<Input> {
+        let (_, y) = stdout.cursor_pos()?;
+        write!(stdout, "\n{}λ ", termion::cursor::Goto(1, y + 1))?;
+        stdout.flush()?;
+
+        while let Some(c) = self.keys.next().transpose()? {
+            let (_, y) = stdout.cursor_pos()?;
+
+            match c {
+                Key::Ctrl('c') => {
+                    println!();
+                    return Ok(Input::Exit);
+                }
+
+                Key::Backspace if self.offset > 0 => {
+                    self.offset -= 1;
+                    self.buffer.remove(self.offset as usize);
+                    write!(
+                        stdout,
+                        "{}{}λ {}{}",
+                        termion::cursor::Goto(1, y),
+                        termion::clear::CurrentLine,
+                        self.buffer,
+                        termion::cursor::Goto(self.start_pos + self.offset, y)
+                    )?;
+                }
+
+                Key::Left if self.offset > 0 => {
+                    self.offset -= 1;
+                    write!(
+                        stdout,
+                        "{}{}λ {}{}",
+                        termion::cursor::Goto(1, y),
+                        termion::clear::CurrentLine,
+                        self.buffer,
+                        termion::cursor::Goto(self.start_pos + self.offset, y)
+                    )?;
+                }
+
+                Key::Right if self.offset < self.buffer.len() as u16 => {
+                    self.offset += 1;
+                    write!(
+                        stdout,
+                        "{}{}λ {}{}",
+                        termion::cursor::Goto(1, y),
+                        termion::clear::CurrentLine,
+                        self.buffer,
+                        termion::cursor::Goto(self.start_pos + self.offset, y)
+                    )?;
+                }
+
+                Key::Char('\n') => {
+                    let line = std::mem::replace(&mut self.buffer, String::new());
+                    let line = line.as_str().trim();
+
+                    if line.is_empty() {
+                        continue;
+                    }
+
+                    self.offset = 0;
+
+                    if let Some(cmd) = line.strip_prefix(":") {
+                        if cmd == "exit" {
+                            return Ok(Input::Exit);
+                        }
+
+                        if cmd.is_empty() {
+                            continue;
+                        }
+
+                        return Ok(Input::Command(cmd.to_string()));
+                    }
+
+                    return Ok(Input::String(line.to_string()));
+                }
+
+                Key::Char(c) => {
+                    self.offset += 1;
+
+                    if self.offset < self.buffer.len() as u16 {
+                        self.buffer.insert((self.offset as usize) - 1, c);
+                        write!(
+                            stdout,
+                            "{}{}λ {}{}",
+                            termion::cursor::Goto(1, y),
+                            termion::clear::CurrentLine,
+                            self.buffer,
+                            termion::cursor::Goto(self.start_pos + self.offset, y)
+                        )?;
+                    } else {
+                        self.buffer.push(c);
+                        write!(
+                            stdout,
+                            "{}{}λ {}",
+                            termion::cursor::Goto(1, y),
+                            termion::clear::CurrentLine,
+                            self.buffer
+                        )?;
+                    }
+                }
+                _ => {}
+            }
+            stdout.flush()?;
+        }
+
+        Ok(Input::Exit)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+mod input;
+
+use eventstore::{Client, ClientSettings, ClientSettingsParseError};
+use input::Input;
+use std::io;
+use std::io::Write;
+use structopt::StructOpt;
+use termion::cursor::DetectCursorPos;
+use termion::raw::IntoRawMode;
+
+#[derive(StructOpt, Debug)]
+struct Params {
+    #[structopt(short = "c",  long = "connection-string", default_value = "esdb://localhost:2113", parse(try_from_str = parse_connection_string))]
+    conn_setts: ClientSettings,
+}
+
+fn parse_connection_string(input: &str) -> Result<ClientSettings, ClientSettingsParseError> {
+    ClientSettings::parse_str(input)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let params = Params::from_args();
+    let mut inputs = crate::input::Inputs::new();
+    // let _client = Client::create(params.conn_setts).await?;
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+
+    loop {
+        let input = inputs.await_input(&mut stdout)?;
+        let (_, y) = stdout.cursor_pos()?;
+
+        match input {
+            Input::Exit => {
+                write!(stdout, "\n{}", termion::cursor::Goto(1, y + 1))?;
+                break;
+            }
+
+            Input::String(line) => {
+                write!(stdout, "\n{}>>> {}", termion::cursor::Goto(1, y + 1), line)?;
+            }
+
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
